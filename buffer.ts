3946b58018a3ffed960f1617a42fba66ace51e66
Godswill Udida <udidagodswill7@gmail.com>
2024-04-24 01:18:19 +0100

    stream

--- buffer.ts
+++ buffer.ts
@@ -7,26 +7,26 @@
 console.log();
 
 import fs from "fs";
-import { number } from "joi";
+import { createReadStream, writeFileSync, createWriteStream } from "fs";
 // import { EventEmitter } from "node:events";
 
 // This creates a new file and write into that new file, the first parameter is the name of the file and the second parameter is the data which you want to write into the file.
 
-// const createFile: void = fs.writeFileSync(
-//   "LargeFile.txt",
-//   "Welcome to my nodejs buffer/stream lesson",
-// );
+const createFile: void = writeFileSync(
+  "LargeFiles.txt",
+  "Welcome to my nodejs buffer/stream lesson"
+);
 
 // console.log(createFile)
 
 // I declared a variable which will help me to read from a file
 
-const readStream = fs.createReadStream("./LargeFile.txt");
+const readStream: fs.ReadStream = createReadStream("LargeFiles.txt");
 
 // This reads the file and returns the contents of the file as a buffer, if we want to see the buffer as a string/plain text we use the toString() method
 
 readStream.on("data", (chunk: string | Buffer) => {
-  console.log(chunk.toLocaleString());
+  console.log(chunk.toString());
 });
 
 // This helps us to know when it has finished reading the stream
@@ -35,9 +35,9 @@ readStream.on("end", () => {
   console.log("Stream has ended");
 });
 
-readStream.on("connection", (stream: string | Buffer) => {
-  console.log("Someone connected", stream);
-});
+// readStream.on("connection", (stream: string | Buffer) => {
+//   console.log("Someone connected", stream);
+// });
 
 // This helps us to know where an error is coming from when it encounters an error during/while reading the stream
 
@@ -45,44 +45,48 @@ readStream.on("error", (err: Error) => {
   console.log(err.stack);
 });
 
-const reader: fs.ReadStream = fs.createReadStream("./LargeFile.txt");
+const reader: fs.ReadStream = createReadStream("./LargeFiles.txt");
 
-const writer: fs.WriteStream = fs.createWriteStream("./write.txt");
+const writer: fs.WriteStream = createWriteStream("./writes.txt");
 
 reader.pipe(writer);
 
-console.log("Piping has ended");
+// console.log("Piping has ended");
 
-const writeToStream: fs.WriteStream = fs.createWriteStream("./LargeFile.txt");
+// const writeToStream: fs.WriteStream = fs.createWriteStream("./LargeFile.txt", );
 
-for (let i = 0; i <= 10; i++) {
-  let sb: string[] = ["st", "nd", "rd", "th"];
 
-  let add = `${sb}`;
-  if (i === 0 ) {
-  } else if (i === 1 || i % 10 === 1) {
-    add = "st";
+
+
+
+
+
+
+for (let i = 0; i <= 100; i++) {
+
+  let add: string = "";
+  if (i === 0) {
+    add = "";
   } else if (i === 1 || i % 10 === 1) {
     add = "st";
   } else if (i === 2 || i % 10 === 2) {
     add = "nd";
-    // console.log(add);
   } else if (i === 3 || i % 10 === 3) {
     add = "rd";
-  } else if (i === 4 && i <= 20) {
-    add = "rd";
+  } else if ((i === 4 && i % 10 === 4) || i > 4) {
+    add = "th";
   } else {
-    add = "rd";
+    console.log("Hmmm");
   }
+  // // console.timeEnd();
 
-    
-    // || 
-    
   console.log(`This is the ${i}${add} time i am writing this `);
-  writeToStream.write(
-    `This is the " + ${i} ${add} + " time i am writing this `
-  );
+
+  // writeToStream.write(`This is the ${i} ${add} time i am writing this`);
 }
+
+
+
 
 // const myEE = new EventEmitter();
 // myEE.on("foo", () => console.log("a"));
